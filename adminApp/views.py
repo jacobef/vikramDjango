5f034fbc8e3f58732662519411d761fe9d5c9ff8
Jacob Friedman <jacobef2@gmail.com>
2022-06-08 21:12:36 -0700

    cleanup, add navbar and base.html

--- adminApp/views.py
+++ adminApp/views.py
@@ -3,14 +3,4 @@ from django.shortcuts import render
 import re
 # Create your views here.
 def users(request):
-    user = 0
-    n = []
-    k = User.objects.all()
-    m = 0
-    for i in range(0, len(k)):
-        n.append(k[m])
-        m += 1
-    t = "*\n".join([str(elem) for elem in n])
-    for users in User.objects.all():
-        user += 1
-    return render(request, "users.html", {"user" : user, "A_users" : User.objects.all(), "T_users" : t})
+    return render(request, "users.html", {"A_users" : User.objects.all(), "users" : len(User.objects.all())})
